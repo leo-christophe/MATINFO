6e5388d6ba8589fe537961ce46349d85591f0f72
Léo Christophe <Leo.Christophe@etu.univ-savoie.fr>
2023-06-13 09:05:09 +0200

    ajout findall materiel -> val

--- MATINFO/Model/ApplicationData.cs
+++ MATINFO/Model/ApplicationData.cs
@@ -7,10 +7,14 @@ namespace Test_WPF_SQL.Model
     public class ApplicationData
     {
         public ObservableCollection<CategorieMateriel> CategoriesMateriel { get; set; }
+        public ObservableCollection<Materiel> lesMateriaux { get; set; }
         public ApplicationData()
         {
             CategorieMateriel e = new CategorieMateriel();
+            Materiel materiel = new Materiel();
+
             CategoriesMateriel = e.FindAll();
+            lesMateriaux = materiel.FindAll();
         }
     }
 }
--- MATINFO/Model/Materiel.cs
+++ MATINFO/Model/Materiel.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MATINFO.Model
 {
-    public class Materiel
+    public class Materiel : Crud<Materiel>
     {
         private int idMateriel;
-        private int FK_idCategorie;
+        private int fK_idCategorie;
         private string ?nomMateriel;
         private string ?referenceConstructeur;
         private string ?codeBarreInventaire;
+
+        public Materiel()
+        {
+        }
+
+        public Materiel(int idMateriel, int fK_idCategorie, string nomMateriel, string referenceConstructeur, string codeBarreInventaire)
+        {
+            this.IdMateriel = idMateriel;
+            this.FK_idCategorie = fK_idCategorie;
+            this.NomMateriel = nomMateriel;
+            this.ReferenceConstructeur = referenceConstructeur;
+            this.CodeBarreInventaire = codeBarreInventaire;
+        }
 
         public int IdMateriel
         {
@@ -27,16 +42,16 @@ namespace MATINFO.Model
             }
         }
 
-        public int FK_idCategorie1
+        public int FK_idCategorie
         {
             get
             {
-                return FK_idCategorie;
+                return fK_idCategorie;
             }
 
             set
             {
-                FK_idCategorie = value;
+                fK_idCategorie = value;
             }
         }
 
@@ -77,6 +92,54 @@ namespace MATINFO.Model
             {
                 codeBarreInventaire = value;
             }
+        }
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Materiel> FindAll()
+        {
+            ObservableCollection<Materiel> lesMateriaux = new ObservableCollection<Materiel>();
+            DataAccess accesBD = new DataAccess();
+            String requete = "select idmateriel, idcategorie, nommateriel, referenceconstructeurmateriel, codebarreinventaire from materiel ;";
+            DataTable datas = accesBD.GetData(requete);
+            if (datas != null)
+            {
+                foreach (DataRow row in datas.Rows)
+                {
+                    Materiel e = new Materiel(
+                        int.Parse(row["idmateriel"].ToString()), 
+                        int.Parse(row["idcategorie"].ToString()), 
+                        (String)row["nommateriel"],
+                        (String)row["referenceconstructeurmateriel"],
+                        (String)row["codebarreinventaire"]
+                        );
+                    lesMateriaux.Add(e);
+                }
+            }
+            return lesMateriaux;
+        }
+
+        public ObservableCollection<Materiel> FindBySelection(string criteres)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
         }
     }
 }

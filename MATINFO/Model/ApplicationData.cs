8e68b729129736d84579da484cad799072edc589
Romain Bajorek <Romain.Bajorek@etu.univ-savoie.fr>
2023-06-13 09:47:48 +0200

    vilider

--- MATINFO/Model/ApplicationData.cs
+++ MATINFO/Model/ApplicationData.cs
@@ -8,13 +8,15 @@ namespace Test_WPF_SQL.Model
     {
         public ObservableCollection<CategorieMateriel> CategoriesMateriel { get; set; }
         public ObservableCollection<Materiel> lesMateriaux { get; set; }
+        public ObservableCollection<Personnel> lesPersonnels { get; set; }
         public ApplicationData()
         {
             CategorieMateriel e = new CategorieMateriel();
             Materiel materiel = new Materiel();
-
+            Personnel personnel = new Personnel();
             CategoriesMateriel = e.FindAll();
             lesMateriaux = materiel.FindAll();
+            lesPersonnels = personnel.FindAll();
         }
     }
 }
---
+++ MATINFO/Model/Personnel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATINFO.Model
+{
+    public class Personnel : Crud<Personnel>
+    {
+        private int idPersonnel;
+        private string nomPersonnel;
+        private string prenomPersonnel;
+        private string emailPersonnel;
+
+        public Personnel()
+        {
+        }
+
+        public Personnel(int idPersonnel, string nomPersonnel, string prenomPersonnel, string emailPersonnel)
+        {
+            this.IdPersonnel = idPersonnel;
+            this.NomPersonnel = nomPersonnel;
+            this.PrenomPersonnel = prenomPersonnel;
+            this.EmailPersonnel = emailPersonnel;
+        }
+
+        public int IdPersonnel
+        {
+            get
+            {
+                return idPersonnel;
+            }
+
+            set
+            {
+                idPersonnel = value;
+            }
+        }
+
+        public string NomPersonnel
+        {
+            get
+            {
+                return nomPersonnel;
+            }
+
+            set
+            {
+                nomPersonnel = value;
+            }
+        }
+
+        public string PrenomPersonnel
+        {
+            get
+            {
+                return prenomPersonnel;
+            }
+
+            set
+            {
+                prenomPersonnel = value;
+            }
+        }
+
+        public string EmailPersonnel
+        {
+            get
+            {
+                return emailPersonnel;
+            }
+
+            set
+            {
+                emailPersonnel = value;
+            }
+        }
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Personnel> FindAll()
+        {
+            ObservableCollection<Personnel> lesPersonnels = new ObservableCollection<Personnel>();
+            DataAccess accesBD = new DataAccess();
+            String requete = "select idpersonnel, nompersonnel, prenompersonnel, emailpersonnel from personnel;";
+            DataTable datas = accesBD.GetData(requete);
+            if (datas != null)
+            {
+                foreach (DataRow row in datas.Rows)
+                {
+                    Personnel e = new Personnel(
+                        int.Parse(row["idpersonnel"].ToString()),
+                        (String)row["nompersonnel"],
+                        (String)row["prenompersonnel"],
+                        (String)row["emailpersonnel"]
+                        );
+                    lesPersonnels.Add(e);
+                }
+            }
+            return lesPersonnels;
+        }
+
+        public ObservableCollection<Personnel> FindBySelection(string criteres)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

d9c4bcb736f9eeba0ff307d4e1ca54d70e3713fb
leolerenard7 <leolerenardo@gmail.com>
2023-06-20 06:55:50 +0200

    OK

---
+++ MATINFOTests/Model/CategorieMaterielTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MATINFO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+
+namespace MATINFO.Model.Tests
+{
+    [TestClass()]
+
+    public class CategorieMaterielTests
+    {
+        [TestMethod()]
+        public void CategorieMaterielTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CategorieMaterielTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CategorieMaterielTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculerNouvelIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindBySelectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
